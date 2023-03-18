85a0583e34de19d83b993518c87295f5238d41bf
IMJGyeongMin <59150207+gyeongent@users.noreply.github.com>
2023-03-18 16:09:59 +0900

    Temporary Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Header from './Components/Header';
 import Main from './Components/Main';
@@ -9,13 +9,11 @@ function App() {
   return (
     <div>
     <Header />
-    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+import '../App.css'
 
 function Header(){
     return(
-        <div>
-            까비
+        <div className="header">
+            <div className="headerLogo">
+                로고
+            </div>
+            <div>
+                <ul className="headerItems">
+                    <li>
+                        <NavLink to='/profile'>
+                            프로필
+                        </NavLink>
+                    </li>
+                    <li>
+                        앨범
+                    </li>
+                    <li>
+                        퀴즈
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
